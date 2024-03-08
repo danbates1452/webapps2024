84cbcc88d72688f0dab00eb6f4bc2b8614ce6e28
Daniel Bates <42376377+danbates1452@users.noreply.github.com>
2024-03-08 09:51:53 +0000

    styling!

--- webapps2024/settings.py
+++ webapps2024/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'bootstrap_customizer',
     'crispy_forms',
     'crispy_bootstrap5',
     'djmoney',
@@ -52,7 +54,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
+    'bootstrap_customizer.middleware.BootstrapThemeMiddleware',
 ]
+
+SITE_ID = 1
 
 ROOT_URLCONF = 'webapps2024.urls'
 
--- webapps2024/urls.py
+++ webapps2024/urls.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from bootstrap_customizer import urls as bootstrap_customizer_urls
+from django.views.generic import RedirectView
+
 from register import views as register_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
+    path('bootstrap_customizer/', include(bootstrap_customizer_urls)),
+    path(r'favicon\.ico', RedirectView.as_view(url='/static/favicon.ico')),
     path('', include('payapp.urls')),
     path('register/', register_views.register_user, name='register'),
     path('login/', register_views.login_user, name='login'),
