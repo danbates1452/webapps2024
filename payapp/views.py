690a832281573f870668d88d85cde66680fb866e
Daniel Bates <42376377+danbates1452@users.noreply.github.com>
2024-03-21 16:37:58 +0000

    untested implementation of send

--- payapp/views.py
+++ payapp/views.py
@@ -1,6 +1,11 @@
+import requests
+import json
+import time
+from django.contrib import messages
 from django.db import transaction
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import requires_csrf_token
+from django.contrib.sites.models import Site
 
 from .forms import SendForm
 from .models import Person, Transaction, Request
@@ -54,23 +59,73 @@ def activity(request):
     return render(request, 'payapp/activity.html', context=context)
 
 
+CURRENCY_CONVERTER_API_URI = Site.objects.get_current().domain + '/conversion/'
+
+
+def call_currency_converter(currency_from, currency_to, amount_from):
+    request_uri = '/'.join([CURRENCY_CONVERTER_API_URI, currency_from, currency_to, amount_from])
+    response = requests.get(request_uri)  # todo: see if this needs a different retry strategy
+    json_response = response.json()
+
+    rate = json_response['data'][0]
+    amount_to = json_response['data'][1]
+    return rate, amount_to
+
+
 @requires_csrf_token
+@transaction.atomic
 def send(request):
     authenticated_area(request.user)
-    # clean the form, and check if they have sufficient money
+    if request.method == 'POST':
+        form = SendForm(request.POST)
+        form.clean()
+        if form.is_valid():
+            from_person = Person.objects.get(form.cleaned_data['from_user'])
+            to_person = Person.objects.get(form.cleaned_data['to_user'])
 
-    #form = MyForm(initial={'charfield1': 'foo', 'charfield2': 'bar'})
+            if not from_person.active:
+                messages.error(request, 'Your account is not active')
+                return render(request, 'payapp/send.html', {'form': form})
+            elif not to_person.active:
+                messages.error(request, 'Recipient account not active')
+                return render(request, 'payapp/send.html', {'form': form})
 
-    form = SendForm(initial={'from_user': request.user.id})
-    context = {'form': form}
-    with transaction.atomic():
-        pass
-        # perform transaction:
-        # subtract 'from' user balance (with converting currency if needed)
-        # add to 'to' user balance (with converted currency)
-        # save new transaction entry to db
+            amount = form.cleaned_data['amount']
 
-    return render(request, 'payapp/send.html', context=context)
+            subtraction_amount = amount
+            addition_amount = amount
+            transaction_currency = form.cleaned_data['amount_currency']
+
+            if transaction_currency != from_person.balance_currency:
+                subtraction_amount = \
+                    call_currency_converter(transaction_currency, from_person.balance_currency, subtraction_amount)[1]
+
+            if transaction_currency != to_person.balance_currency:
+                addition_amount = \
+                    call_currency_converter(transaction_currency, to_person.balance_currency, subtraction_amount)[1]
+
+            if from_person.balance >= subtraction_amount:  # perform transaction:
+                # subtract 'from' user balance (with converting currency if needed) and save
+                from_person.balance -= subtraction_amount
+                from_person.save()
+
+                # add to 'to' user balance (with converted currency) and save
+                to_person.balance += addition_amount
+                to_person.save()
+
+                # save new transaction entry to db
+                form.save()
+
+                messages.success(request, f'Payment Successful: you have sent {amount} to {to_person.user.username}')
+                return render(request, 'payapp/send.html')
+            else:
+                messages.error(request, "Insufficient funds for transaction")
+                return render(request, 'payapp/send.html', {'form': form})
+        else:
+            return render(request, 'payapp/send.html', {'form': form})
+    else:
+        form = SendForm(initial={'from_user': request.user.id})
+        return render(request, 'payapp/send.html', {'form': form})
 
 
 @requires_csrf_token
