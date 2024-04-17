bb5293608e09794c77d6884f98710b66deaefd88
Daniel Bates <42376377+danbates1452@users.noreply.github.com>
2024-04-17 21:35:56 +0100

    add decorators to timestamp view + remove unused imports

--- conversion/views.py
+++ conversion/views.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ValidationError
 from decimal import Decimal
 
 from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
 currency_map = {
--- timestampapp/views.py
+++ timestampapp/views.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
 
@@ -9,5 +9,7 @@ def get_current_posix_timestamp():
     return datetime.timestamp(datetime.now())
 
 
+@api_view(('GET', ))
+@renderer_classes((JSONRenderer,))
 def timestamp_view(request):
     return Response(data=get_current_posix_timestamp())
